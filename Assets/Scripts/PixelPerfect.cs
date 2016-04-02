64c4d56f91db9161ad9fda7253eb18cd0236f125
barzb <barzb@hochschule-trier.de>
2016-04-02 22:25:35 +0200

    Add PixelPerfect Script

---
+++ Assets/Scripts/PixelPerfect.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PixelPerfect : MonoBehaviour
+{
+    private static int GridSize = 64;
+    
+
+    // set new position
+	public void MoveTo(Vector2 target)
+    {
+        transform.position = Align(target);
+    }
+
+    // set new position
+    public void MoveTo(float x, float y)
+    {
+        transform.position = Align(new Vector2(x, y));
+    }
+
+    // move towards a target with specified speed
+    public void MoveTowards(Vector2 direction, float speed)
+    {
+        float dt = Time.deltaTime;
+
+        // TODO  (Auf collision achten! RigidBody?)
+    }
+
+
+
+    // Align coordinates to pixel perfect coordinates (depending on grid size)
+
+    public static Vector2 Align(Vector2 pos)
+    {
+        pos.x = Align(pos.x);
+        pos.y = Align(pos.y);
+
+        return pos;
+    }
+
+    public static float Align(float source)
+    {
+        return Mathf.Floor(source / GridSize) * GridSize;
+    }
+}
