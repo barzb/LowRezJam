3bef566bc36eaad25f3cac510efd57a35aab6e68
barzb <barzb@hochschule-trier.de>
2016-04-03 00:49:29 +0200

    Mask the map (fog of war like)

---
+++ Assets/Scripts/Echolot/EchoCaster.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EchoCaster : MonoBehaviour
+{
+    public Grid grid;
+    public int echoRange = 10;
+    
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    private void CastEcho()
+    {
+
+    }
+}
