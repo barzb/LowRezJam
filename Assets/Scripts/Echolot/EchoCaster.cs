b3568bfe053aefcd722dee9b4ceb783c79ba5666
barzb <barzb@hochschule-trier.de>
2016-04-03 18:27:56 +0200

    Echolot Done!

--- Assets/Scripts/Echolot/EchoCaster.cs
+++ Assets/Scripts/Echolot/EchoCaster.cs
@@ -3,20 +3,60 @@ using System.Collections;
 
 public class EchoCaster : MonoBehaviour
 {
-    public int echoRange = 10;
+    public int   echoRange;    // in pixels
+    public float echoSpeed;    // in 1/x seconds
+    public float echoCooldown; // in seconds
+
+    // for cooldown
+    private float nextActivation = 0f;
     
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
 	// Update is called once per frame
-	void Update () {
-	
+	void Update ()
+    {
+	    if(Input.GetButton("Action1"))
+        {
+            CastEcho();
+        }
 	}
 
+    // cast an echo
     private void CastEcho()
     {
+        if (Time.time < nextActivation) return;
 
+        nextActivation = Time.time + echoCooldown;
+        
+        StartCoroutine(Circle(Map.Instance.Fog, Map.Instance.visibleColor));
+    }
+
+    // slowly expand the circle
+    public IEnumerator Circle(Texture2D tex, Color col)
+    {
+        for (int r = 0; r < echoRange; r++)
+        {
+            int offsetX = (int)transform.position.x;
+            int offsetY = (int)transform.position.y;
+            int x, y, px, nx, py, ny, d;
+
+            for (x = 0; x <= r; x++)
+            {
+                d = Mathf.RoundToInt(Mathf.Sqrt(r * r - x * x));
+                for (y = 0; y <= d; y++)
+                {
+                    px = offsetX + x; // +x
+                    nx = offsetX - x; // -x
+                    py = offsetY + y; // +y
+                    ny = offsetY - y; // -y
+
+                    // set a pixel in every quater of the circle
+                    tex.SetPixel(px, py, col);
+                    tex.SetPixel(nx, py, col);
+                    tex.SetPixel(px, ny, col);
+                    tex.SetPixel(nx, ny, col);
+                }
+            }
+            tex.Apply();
+            yield return new WaitForSeconds(1f/echoSpeed);
+        }
     }
 }
---
+++ Assets/Scripts/Map.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Map : MonoBehaviour
+{
+    private static Map instance;
+    
+    private bool[,]   map;    // true = air; false = collision
+    private Texture2D fog;
+    private Sprite    fogSprite;
+	
+    // PROPERTIES
+    public Texture2D Fog { get { return fog; } }
+    public static Map Instance { get { return instance; } }
+
+    // INSPRECTOR ATTRIBUTES
+    public Color fogOfWarColor;
+    public Color visibleColor;
+    public int mapWidth;
+    public int mapHeight;
+
+    void Start()
+    {
+        // set static reference
+        instance = this;        
+        
+        // create the fog
+        CreateFogTexture();
+        fogSprite = Sprite.Create(fog, new Rect(Vector2.zero, new Vector2(fog.width, fog.height)), new Vector2(0f, 0f), 1);
+        GetComponent<SpriteRenderer>().sprite = fogSprite;
+
+        map = new bool[mapWidth, mapHeight];
+        // @BINA: Hier die Funktion einfügen, die das Terrain prozedural generiert
+        // benutze "map" für kollisionen; also die passierbaren felder = true
+    }
+
+    // create the fog texture which masks the terrain
+    // can be made visible with EchoCaster
+    public void CreateFogTexture()
+    {
+        fog = new Texture2D(mapWidth, mapHeight);
+
+        // No Anti-Aliasing! (for PixelPerfect display)
+        fog.filterMode = FilterMode.Point;
+
+        // set every pixel black
+        for(int i = 0; i < mapWidth; i++)
+        {
+            for(int j = 0; j < mapHeight; j++)
+            {
+                fog.SetPixel(i, j, fogOfWarColor);
+            }
+        }
+        // apply changes to texture
+        fog.Apply();
+    }
+
+
+    void Update()
+    {
+
+    }
+}
